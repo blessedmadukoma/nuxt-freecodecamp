84a30a3b7c390d8acb33a02dfb16cc82c58d9f54
blessedmadukoma <blessedmadukoma@gmail.com>
2023-12-27 14:34:54 +0100

    feature pages📑: routing, links and IDs

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Hello Nuxt 3</div>
+  <div>
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="events">vents</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
---
+++ pages/events/[id].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  const nuxt = useNuxtApp();
+
+  console.log(nuxt);
+</script>
+
+<template>
+  <div>Events ID: {{ $route.params.id }}</div>
+</template>
+
+<style scoped></style>
