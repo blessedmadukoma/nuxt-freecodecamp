f247e81ba33cee619ecc453e0dc33c2f9f3680b6
blessedmadukoma <blessedmadukoma@gmail.com>
2024-01-08 13:34:50 +0100

    feature hooks🪝: config, plugins and trigger

---
+++ pages/profile.vue
@@ -0,0 +1,11 @@
+<script setup>
+  useHead({
+    title: "Profile page",
+  });
+</script>
+
+<template>
+  <div class="container py-3 mx-auto Main">
+    <h1 class="text-teal-500">Profile</h1>
+  </div>
+</template>
